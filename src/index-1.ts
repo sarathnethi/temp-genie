00a74e3cc099ba54142c0124d8bb348fef7ffc17
Nethi sarath Chandra <nethisarath.chandra@hcl.com>
2025-12-23 03:53:38 +0530

    Added sample code in index-1

--- src/index-1.ts
+++ src/index-1.ts
@@ -2,3 +2,12 @@
 export const greet = (name: string): string => {
     return `Hello, ${name}!`;
 }
+export const farewell = (name: string): string => {
+    return `Goodbye, ${name}!`;
+}
+export const thankYou = (name: string): string => {
+    return `Thank you, ${name}!`;
+}
+export const welcome = (name: string): string => {
+    return `Welcome, ${name}!`;
+}
